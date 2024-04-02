207a0c81c18b0d674aaabe6297d94c491f121a0c
Roman Tretyakov <crazytaro@gmail.com>
2024-04-02 16:25:34 +0300

    pagination

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const CategoriesArr = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
-function Categories() {
-    const [selectedCat, selectedCatSet] = React.useState(0);
+function Categories({ categotyId, categotyIdSet }) {
+    //const [selectedCat, selectedCatSet] = React.useState(0);
+    const OnClickCat = (index) => {
+        if (categotyId !== index)
+            categotyIdSet(index);
 
+    }
     return (
         <div className="categories">
 
             <ul>
                 {CategoriesArr.map((item, index) => {
-                    return <li key={index} className={selectedCat === index ? 'active' : ''} onClick={() => selectedCatSet(index)
+                    return <li key={index} className={categotyId === index ? 'active' : ''} onClick={() => OnClickCat(index)
                     }> {item}</li>
                 })}
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,24 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from "../components/PizzaBlock/skeleton";
 import Sort from '../components/Sort';
 import Categories from '../components/Categories';
+import Pagination from '../components/Pagination';
 const Home = () => {
     const [productsArr, productsArrUpdate] = React.useState([]);
     const [isLoading, isLoadingSet] = React.useState(true);
+    const [categotyId, categotyIdSet] = React.useState(1);
+    const [currentPage, setcurrentPage] = React.useState(1);
     React.useEffect(() => {
         isLoadingSet(true);
-        fetch('https://65d7103d27d9a3bc1d7a0dda.mockapi.io/pizza_site')
+        //const categoty = categotyId > 0 ? `category=${categotyId}` : ``;
+        const url = new URL('https://65d7103d27d9a3bc1d7a0dda.mockapi.io/pizza_site');
+        url.searchParams.append('page', currentPage);
+        url.searchParams.append('limit', 4);
+        categotyId && url.searchParams.append('category', categotyId);
+        fetch(url, {
+            method: 'GET',
+            headers: { 'content-type': 'application/json' },
+        })
+            // fetch(`https://65d7103d27d9a3bc1d7a0dda.mockapi.io/pizza_site?${category}`)
             .then((response) => response.json())
             .then((json) => {
                 productsArrUpdate(json);
@@ -19,12 +31,12 @@ const Home = () => {
 
         //axios.get('https://65d7103d27d9a3bc1d7a0dda.mockapi.io/pizza_site').then(res => productsInCartUpdate(res.data));
     },
-        [] //second arg of useEffect empty array, we need fetch to be called only once, in app start componentDidMount
+        [categotyId, currentPage]
     );
     return (<>
 
         <div className="content__top">
-            <Categories />
+            <Categories categotyId={categotyId} categotyIdSet={categotyIdSet} />
             <Sort />
         </div>
         <h2 className="content__title">Все пиццы</h2>
@@ -34,6 +46,8 @@ const Home = () => {
                 productsArr.map(obj => <PizzaBlock key={obj.id} {...obj} />)
             }
         </div>
+        <Pagination onChangePage={number => setcurrentPage(number)} />
+
 
     </>);
 }
